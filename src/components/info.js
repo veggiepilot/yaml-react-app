2e59c6265295df361d8cc742b074ac452204f92b
veggiepilot <johanneschitura@gmail.com>
2021-12-18 23:32:51 -0500

    Iframe Component

--- src/components/info.js
+++ src/components/info.js
@@ -1,3 +1,5 @@
+import Iframe from './iframe'
+
 const Info = props => {
     const { info } = props
     const { Name, wTeaser, wUrl, yUrl } = info
@@ -5,12 +7,20 @@ const Info = props => {
         <div className="row">
             {yUrl && (
                 <div className="column">
-                    <iframe src={yUrl}/>
+                    <Iframe src={yUrl}/>
                 </div>
             )}
             <div className="column">
                 <h2>You searched: {Name}</h2>
                 <p>{wTeaser}</p>
+                <a 
+                    href={wUrl} 
+                    target="_blank" 
+                    rel="noreferrer noopener"
+                    className="button"
+                >
+                    LEARN MORE
+                </a>
             </div>
         </div>
     )
