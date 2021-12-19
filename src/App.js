b897c9ceb4e8f0f8772b13ee0f5bd7af2fda047d
veggiepilot <johanneschitura@gmail.com>
2021-12-18 22:19:52 -0500

    API module

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css';
 import Header from './components/header';
 import SearchForm from './components/searchForm'
+import API from './utils/API'
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState('')
+  const [searchTerm, setSearchTerm] = useState('Tame Impala')
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    getRecommendations()
+  }, [])
+
+  const getRecommendations = async () => {
+    setLoading(true)
+    try {
+      const results = await API.getRecommendations(searchTerm)
+      console.log(results)
+    } catch (err) {
+      alert(err)
+    }
+    setLoading(false)
+  }
 
   return (
     <>
@@ -14,6 +31,9 @@ function App() {
           setSearchTerm={setSearchTerm}
         />
       </Header>
+      <div className="container">
+        
+      </div>
     </>
   );
 }
